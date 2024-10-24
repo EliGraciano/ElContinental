d88ea680c24aa57a8afbdf59edc641406d3dfffd
Elias Graciano <eli162004@gmail.com>
2024-10-24 16:45:11 -0300

    avances en logica tercia

--- src/Continental/modelo/Escalera.java
+++ src/Continental/modelo/Escalera.java
@@ -1,12 +1,36 @@
 package Continental.modelo;
 
 import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.LinkedList;
 
 public class Escalera  implements ICombinacion{
 
     @Override
     public boolean esValida(ArrayList<Carta> cartas){
-        //TODO hacer algoritmo de verificacion de escalera
+        //TODO hacer algoritmo de verificacion de escalera tineen que ser si o si de 4 cartas
+        // primero ordeno el array, y despues me fijo como esta ordenado(si el array me queda ordenado continuo es una escalera)
+        if (cartas.size() != 4){return false;}
+        Collections.sort(cartas, Comparator.comparingInt(Carta::getValor));
+        int cartavalor = cartas.getFirst().getValor();
+        for (Carta carta : cartas){
+            if (carta.getPalo() != Palo.MONO ) {
+                Palo paloescalera = cartas.getFirst().getPalo();
+            }
+
+
+
+        }
+
+
+
+        }
+
+
+
+
         return true;
     }
+
 }
--- src/Continental/modelo/Tercia.java
+++ src/Continental/modelo/Tercia.java
@@ -1,14 +1,30 @@
 package Continental.modelo;
 
 import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
 
 public class Tercia implements ICombinacion{
 
+
     @Override
     public boolean esValida(ArrayList<Carta> cartas) {
-        // Necesitamos exactamente tres cartas
-        // TODO Hacer metodo de verificacion
-        if (cartas.size() != 3) return false;
+        // pongo todos los palos en un array, y por cada carta sumo 1 al palo en dicho array, y al salir ninguno tiene que tener mas de 1,sino hay 2 cartas de un mismo palo
+        if (cartas.size() != 3) {return false;}
+        int valorCartas = cartas.getFirst().getValor();
+        int[] contadorPalos = new int[Palo.values().length];
+        for (Carta carta : cartas){
+            if(valorCartas != carta.getValor()){
+                return false;
+            }
+            Palo palo = carta.getPalo();
+            contadorPalos[palo.ordinal()]++;
+        }
+        for (int count : contadorPalos) {
+            if (count > 1) {
+                return false; // Si algún palo aparece más de una vez, no es tercia
+            }
+        }
         return true;
     }
     
